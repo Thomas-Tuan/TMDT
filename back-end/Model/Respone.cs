06563aeb63b065244631570abe11ff8bf8565ff9
Trọng Tuấn <hinchuon@gmail.com>
2023-11-23 04:40:20 +0700

    remove back-end file

--- back-end/Model/Respone.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FurnitureShop.Model
-{
-    public class Respone
-    {
-        public int? Status { get; set; }
-        public string? Message { get; set; }
-    }
-}
--- back-end/Model/UserRoles.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FurnitureShop.Model
-{
-    public static class UserRoles
-    {
-        public const string User = "User";
-        public const string Admin = "Admin";
-    }
-}
